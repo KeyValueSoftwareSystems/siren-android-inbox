074ca4126113ac86cc7bfca1a6264e85efa25f6e
varghese-babu <varghese@keyvalue.systems>
2024-03-22 16:29:30 +0530

    feat: Add environment configuration changes

--- siren-sdk/src/main/java/com/keyvalue/siren/androidsdk/presenter/BasePresenter.kt
+++ siren-sdk/src/main/java/com/keyvalue/siren/androidsdk/presenter/BasePresenter.kt
@@ -3,14 +3,16 @@ package com.keyvalue.siren.androidsdk.presenter
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import com.keyvalue.siren.androidsdk.R
 
-abstract class BasePresenter {
-    private var baseContext: Context? = null
-    protected var baseURL = "https://api.dev.sirenapp.io/"
+abstract class BasePresenter(
+    private var context: Context,
+) {
+    protected var baseURL = context.resources.getString(R.string.SIREN_BASE_URL_DEV)
 
     protected fun isNetworkAvailable(): Boolean {
         val connectivityManager =
-            baseContext?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetwork ?: return false
         val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
         return when {
