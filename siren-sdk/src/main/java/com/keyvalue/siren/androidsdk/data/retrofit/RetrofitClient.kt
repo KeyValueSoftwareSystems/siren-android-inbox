aa3131db4b4427c1a19b4151b2f3df83075061af
varghese-babu <varghese@keyvalue.systems>
2024-03-25 16:32:08 +0530

    refactor: Remove logging interceptor

--- siren-sdk/src/main/java/com/keyvalue/siren/androidsdk/data/retrofit/RetrofitClient.kt
+++ siren-sdk/src/main/java/com/keyvalue/siren/androidsdk/data/retrofit/RetrofitClient.kt
@@ -1,7 +1,6 @@
 package com.keyvalue.siren.androidsdk.data.retrofit
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -13,13 +12,10 @@ class RetrofitClient {
         fun getRetrofitInstance(baseUrl: String): Retrofit? {
             if (retrofit == null && baseUrl.isNotEmpty()) {
                 var absoluteBaseUrl = baseUrl
-                val loggingInterceptor = HttpLoggingInterceptor()
-                loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
                 if (absoluteBaseUrl[baseUrl.length - 1] != '/') absoluteBaseUrl = "$baseUrl/"
                 val okHttpClientOptions =
                     OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS)
                         .connectTimeout(30, TimeUnit.SECONDS)
-                        .addInterceptor(loggingInterceptor)
 
                 retrofit =
                     Retrofit.Builder().baseUrl(
